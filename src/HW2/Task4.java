2652d71acac7819275461f4b5020760b522e856e
MarynaSkrypnyk <Marikaya.1997@gmail.com>
2023-10-19 23:44:57 +0300

    HW2 Task3

---
+++ src/HW2/Task4.java
@@ -0,0 +1,29 @@
+package HW2;
+
+import java.util.Scanner;
+public class Task4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int d;
+        int f;
+        int g;
+        System.out.println("Enter first number: ");
+        d = sc.nextInt();
+        System.out.println("Enter second number: ");
+        f = sc.nextInt();
+        System.out.println("Enter third number: ");
+        g = sc.nextInt();
+        if ((Math.abs(d)) < (Math.abs(f)) && (Math.abs(d)) < (Math.abs(g))) {
+            System.out.println ("Number smaller in modulus " + Math.abs(d));
+        } else if ((Math.abs(f)) < (Math.abs(d)) && (Math.abs(f)) < (Math.abs(g))) {
+            System.out.println ("Number smaller in modulus " + Math.abs(f));
+        } else {
+            System.out.println("Number smaller in modulus " + Math.abs(g));
+        }
+
+
+
+
+
+    }
+}
