0edaa8f84e2abf9387f4199579f8ddab1a29656b
MarynaSkrypnyk <Marikaya.1997@gmail.com>
2023-10-31 10:27:08 +0200

    HW9

---
+++ src/ua.skrypnyk.hw9/Dog.java
@@ -0,0 +1,18 @@
+package src.ua.skrypnyk.hw9;
+
+public class Dog extends Animal {
+    private int hourseOfSleep;
+    private String kindOfFood;
+    public Dog(String kindOfFood, int hourseOfSleep) {
+        super(kindOfFood, hourseOfSleep);
+        this.kindOfFood = kindOfFood;
+        this.hourseOfSleep = hourseOfSleep;
+    }
+    @Override
+    public void eat() {
+        System.out.println("Birds");
+    }
+    public final void walk(){
+        System.out.println("walk");
+    }
+}
