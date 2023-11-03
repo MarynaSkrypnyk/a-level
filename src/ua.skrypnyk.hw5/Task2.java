e8e0b907b17591fa72096d58924486383677e7f2
MarynaSkrypnyk <Marikaya.1997@gmail.com>
2023-11-03 22:43:20 +0200

    changes in HW5 Task2

---
+++ src/ua.skrypnyk.hw5/Task2.java
@@ -0,0 +1,29 @@
+package src.ua.skrypnyk.hw5;
+
+import java.util.Arrays;
+
+public class Task2 {
+
+    public static void main(String[] args) {
+        int[] array = new int[20];
+        fill(array);
+        isSorted(array);
+        System.out.println(isSorted(array));
+    }
+
+    private static void fill(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = ((int) (Math.random() * 20) + 1);
+        }
+        System.out.println(Arrays.toString(array));
+    }
+
+    private static boolean isSorted(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < array[i + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
