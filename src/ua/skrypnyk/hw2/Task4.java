69c34350ebd3474c9d430f94e682ee0aa75b82c3
MarynaSkrypnyk <Marikaya.1997@gmail.com>
2023-11-03 13:23:42 +0200

    changes in HW2 Task2, Task3, Task4

---
+++ src/ua/skrypnyk/hw2/Task4.java
@@ -0,0 +1,19 @@
+package src.ua.skrypnyk.hw2;
+
+import java.util.Scanner;
+public class Task4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int d;
+        int f;
+        int g;
+        System.out.println("Enter first number: ");
+        d = sc.nextInt();
+        System.out.println("Enter second number: ");
+        f = sc.nextInt();
+        System.out.println("Enter third number: ");
+        g = sc.nextInt();
+
+        System.out.println(d > g? d > f ? "Fist number bigger" : "Third number bigger": f > g ?"Second number bigger" : "Third number bigger");
+    }
+}
