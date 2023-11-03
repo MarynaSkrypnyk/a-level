2aec932b21e6f053a0e80a98d97b3774518f1a65
MarynaSkrypnyk <Marikaya.1997@gmail.com>
2023-11-03 21:09:30 +0200

    changes in HW3 Task3

--- src/ua.skrypnyk.hw3/Task3.java
+++ src/ua.skrypnyk.hw3/Task3.java
@@ -1,22 +1,36 @@
-package ua.skrypnyk.hw3;
+package src.ua.skrypnyk.hw3;
 
 import java.util.Scanner;
 
 public class Task3 {
     public static void main(String[] args) {
+        scannerWords();
+    }
 
+    public static void scannerWords() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a few words separated by spaces");
         String input = sc.nextLine();
+        String[] words = input.split("\\s+");
         int count = 0;
-        if (input.length() != 0) {
-            count++;
-            for (int i = 0; i < input.length(); i++) {
-                if (input.charAt(i)  == ' ') {
-                    count++;
-                }
+        for (String word : words) {
+            if (isValidWord(word)) {
+                count++;
             }
-            System.out.println("You write " + count + " word");
         }
+        System.out.println("You enter " + count+ " words");
     }
-}
+
+    static boolean isValidWord(String word) {
+        char[] chars = word.toCharArray();
+        for (char c : chars) {
+            if (!Character.isAlphabetic(c)) {
+                return false;
+            }
+        }
+        return true;
+    }
+        }
+
+
+
