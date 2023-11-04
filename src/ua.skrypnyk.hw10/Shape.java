b60b9a2b36e08a6b2e8c64fb6810a49d43b551e1
MarynaSkrypnyk <Marikaya.1997@gmail.com>
2023-11-04 21:48:24 +0200

    work on mistakes

---
+++ src/ua.skrypnyk.hw10/Shape.java
@@ -0,0 +1,31 @@
+package src.ua.skrypnyk.hw10;
+
+public class Shape {
+
+    private double perimeter;
+    private double area;
+
+    public Shape (double perimeter, double area) {
+        this.perimeter = perimeter;
+        this.area = area;
+    }
+
+    public void perimeter () {
+        System.out.println("Perimeter "+ perimeter);
+    }
+
+    public double area (){
+        System.out.println("Area "+ area);
+        return area;
+    }
+
+    public double getPerimeter() {
+        return perimeter;
+    }
+
+    public double getArea() {
+        return area;
+    }
+    Shape (){};
+
+}
