441b2430483bb256eff03d592c532d211c0c68bf
MarynaSkrypnyk <Marikaya.1997@gmail.com>
2023-11-08 15:12:42 +0200

    change HW2 Task 2

--- src/ua.skrypnyk.hw2/Task2.java
+++ src/ua.skrypnyk.hw2/Task2.java
@@ -1,19 +1,19 @@
 package src.ua.skrypnyk.hw2;
 
 public class Task2 {
-    public static double x1 = 1.0;
-    public static double x2 = 3.0;
-    public static double x3 = 6.0;
-    public static double y1 = 5.0;
-    public static double y2 = 5.0;
-    public static double y3 = 7.0;
-
     public static void main(String[] args) {
-        squareTriangle();
-        System.out.println(squareTriangle());
+        double x1 = 1.0;
+        double x2 = 3.0;
+        double x3 = 6.0;
+        double y1 = 5.0;
+        double y2 = 5.0;
+        double y3 = 7.0;
+
+        calculationSquareTriangle(x1, x2, x3, y1, y2,y3);
+        System.out.println();
     }
 
-    private static double squareTriangle (){
+    private static void calculationSquareTriangle (double x1,double x2, double x3, double y1, double y2, double y3){
 
     double lenghtA = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
     double lenghtB = Math.sqrt(Math.pow(x3 - x2, 2) + Math.pow(y3 - y2, 2));
@@ -23,7 +23,7 @@ public class Task2 {
     double perimeterHalf = perimeter / 2.0;
     double squareTriangle = Math.sqrt(perimeterHalf * (perimeterHalf - lenghtA) * (perimeterHalf - lenghtB) * (perimeterHalf - lenghtC));
 
-    return squareTriangle;
+        System.out.println(squareTriangle);
 
     }
 }
