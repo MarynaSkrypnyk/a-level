d9917e06f3a779eeb33b41606ce2b7343c410b8f
MarynaSkrypnyk <Marikaya.1997@gmail.com>
2023-11-10 20:25:41 +0200

    HW4 Task 3

--- src/ua.skrypnyk.hw4/Task3.java
+++ src/ua.skrypnyk.hw4/Task3.java
@@ -1,25 +1,31 @@
 package src.ua.skrypnyk.hw4;
 
 import java.util.Arrays;
+//считает верно, но нужно в методе exchangeNul создать копию массива чтобы изначальный массив не изменился
+//и вернуть этот массив из метода, вывести его в main
 public class Task3 {
-        public static void main(String[] args) {
-            int[] array = new int[2000];
-            fill(array);
-            exchangeNul (array);
-        }
-    private static void fill(int[] array) {
-      for (int i = 0; i < array.length; i++) {
-      array[i] = (int)(Math.random()*2000);
-      }
-      System.out.println(Arrays.toString(array));
-      }
+    public static void main(String[] args) {
+        int[] array = new int[2000];
+        fill(array);
+        exchangeNull(array);
 
-    private static void exchangeNul(int[] array) {
-            for (int i = 0; i < array.length; i++) {
-                if (array[i] % 2 == 0) {
-                    array[i] = 0;
-                }
-            }
-            System.out.println(Arrays.toString(array));
+        System.out.println(Arrays.toString(array));
+        System.out.println(Arrays.toString(exchangeNull(array)));
+    }
+
+    private static void fill(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 2000);
         }
+    }
+
+    private static int[] exchangeNull(int[] array) {
+        int [] arrayCopy = Arrays.copyOf(array, array.length);
+        for (int i = 0; i < arrayCopy.length; i++) {
+            if (arrayCopy[i] % 2 == 0) {
+                arrayCopy[i] = 0;
+            }
+        }
+        return arrayCopy;
+    }
 }
