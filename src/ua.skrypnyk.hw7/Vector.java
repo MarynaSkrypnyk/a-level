4f5846b63cad5e97f4f2fe475775d23d51e94022
MarynaSkrypnyk <Marikaya.1997@gmail.com>
2023-11-22 12:21:39 +0200

    work on mistake hw7 Vectors

--- src/ua.skrypnyk.hw7/Vector.java
+++ src/ua.skrypnyk.hw7/Vector.java
@@ -6,62 +6,57 @@ public class Vector {
     public int x;
     public int y;
     public int z;
-
-    public Vector (int x, int y, int z) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
+    public Vector (int ax, int ay, int az) {
+        this.x = ax;
+        this.y = ay;
+        this.z = az;
     }
+
     public static void main(String[] args) {
-        int bx = 4;
-        int by = 6;
-        int bz = 10;
+
         Vector vector = new Vector(3,5,9);
-        System.out.println("Count vector length "+ vector.countVectorLength(vector.x, vector.y, vector.z));
 
-        int [] vectorProduct = countVectorProduct(vector.x, vector.y, vector.z, bx, by, bz);
-        System.out.println("Vector product with another vector" + Arrays.toString(vectorProduct));
-        System.out.println("Count cosinus between two vector " + vector.cosinusBetweenTwoVector(vector.x, vector.y, vector.z, bx, by, bz));
-
-        int [] sumVectors = sumVectors(vector.x, vector.y, vector.z, bx, by, bz);
-        System.out.println("Sum vectors " + Arrays.toString(sumVectors));
-
-        int [] differenceVectors = differenceVectors(vector.x, vector.y, vector.z, bx, by, bz);
-        System.out.println("Difference vectors " + Arrays.toString(differenceVectors));
-
+        System.out.println("vector length "+ vector.countVectorLength(vector));
+        System.out.println("countVectorProduct"+ vector.countVectorProduct(vector, 2,2,2));
+        System.out.println("cosinus between two vector " + vector.cosinusBetweenTwoVector(vector,2,2,2));
+        System.out.println("sumVectors" + vector.sumVectors(vector,2,2,2));
+        System.out.println("differenceVectors"+vector.differenceVectors(vector,2,2,2));
         Vector [] vectors = generate(2);
         System.out.println(Arrays.toString(vectors));
 
     }
 
-    public double countVectorLength(int x, int y, int z){
+    public double countVectorLength(Vector vector){
         double vectorLength = Math.sqrt(Math.pow(x,2)+ Math.pow(y,2)+ Math.pow(z,2));
         return vectorLength;
     }
-
-    public static int [] countVectorProduct(int x, int y, int z, int bx, int by, int bz){
-        int producktPointA = Math.abs(y * bz - z * by);
-        int producktPointY = Math.abs(z * bx - x * bz);
-        int producktPointZ = Math.abs(x * by - y * bx);
-        return new int[] {producktPointA, producktPointY, producktPointZ};
+    public Vector countVectorProduct(Vector vector, int bx, int by, int bz) {
+        return new Vector(
+                bz * vector.y - by * vector.z,
+                bx * vector.z - bz * vector.x,
+                by * vector.x - bx * vector.y
+        );
     }
-    public double cosinusBetweenTwoVector (int x, int y, int z, int bx, int by, int bz){
+    public double cosinusBetweenTwoVector (Vector vector, int bx, int by, int bz){
         int sklarProduct = x * bx + y * by + z * bz;
         double vectorLengthSeconVector = Math.sqrt(Math.pow(bx,2)+ Math.pow(by,2)+ Math.pow(bz,2));
-        double cosinusBetweenTwoVector = (sklarProduct/(countVectorLength(x, y, z) * vectorLengthSeconVector));
+        double cosinusBetweenTwoVector = (sklarProduct/(countVectorLength(vector) * vectorLengthSeconVector));
         return cosinusBetweenTwoVector;
     }
-    public static int [] sumVectors(int x, int y, int z, int bx, int by, int bz){
-        int sumVectorsPointA = x + bx;
-        int sumVectorsPointY = y + by;
-        int sumVectorsPointZ = z + bz;
-        return new int [] {sumVectorsPointA, sumVectorsPointY, sumVectorsPointZ};
+    public Vector sumVectors(Vector vector, int bx, int by, int bz) {
+        return new Vector(
+                bx + vector.x,
+                by + vector.y,
+                bz + vector.z
+        );
     }
-    public static int [] differenceVectors(int x, int y, int z, int bx, int by, int bz){
-        int differenceVectorsPointA = x - bx;
-        int differenceVectorsPointY = y - by;
-        int differenceVectorsPointZ = z - bz;
-        return new int [] {differenceVectorsPointA, differenceVectorsPointY, differenceVectorsPointZ};
+
+    public Vector differenceVectors(Vector vector, int bx, int by, int bz) {
+        return new Vector(
+                vector.x - bx,
+                vector.y - by,
+                vector.z - bz
+        );
     }
 
     public static Vector[] generate(int n){
