657785953d7986340e4ea9c671500070458b3dd4
MarynaSkrypnyk <Marikaya.1997@gmail.com>
2023-11-24 00:05:05 +0200

    HW13 Comparator Shapes

---
+++ src/ua/skrypnyk/hw13/Main.java
@@ -0,0 +1,46 @@
+package src.ua.skrypnyk.hw13;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        Comparator <Shape> perimeterShapesComparator = (Shape o1, Shape o2) -> {
+//            return (int) (o1.area() - o2.area());
+//        };
+//        Comparator <Shape> areaShapesComparator = (Shape o1, Shape o2) -> {
+//            return (int) (o1.area() - o2.area());
+//        };
+
+        PerimeterShapesComparator perimeterShapesComparator = new PerimeterShapesComparator();
+        AreaShapesComparator areaShapesComparator = new AreaShapesComparator();
+        areaShapesComparator.thenComparing(perimeterShapesComparator);
+
+        Set<Shape> shapes = new TreeSet<>(areaShapesComparator);
+
+        Circle circl = new Circle(3);
+        Triangle triangle = new Triangle(2,1,1,3.2);
+        Square square = new Square(4);
+
+        shapes.add(circl);
+        shapes.add(triangle);
+        shapes.add(square);
+
+        List<Shape> shapeList = new ArrayList<>();
+        shapeList.add(circl);
+        shapeList.add(triangle);
+        shapeList.add(square);
+
+        Collections.sort(shapeList, perimeterShapesComparator);
+
+        System.out.println(perimeterShapesComparator);
+        System.out.println(areaShapesComparator);
+        System.out.println(shapeList);
+
+        System.out.println(shapes);
+
+
+    }
+}
+
+
---
+++ src/ua/skrypnyk/hw13/Square.java
@@ -0,0 +1,38 @@
+package src.ua.skrypnyk.hw13;
+
+import java.util.Comparator;
+
+    public class Square extends Shape implements Comparator<Square> {
+        private double perimeter;
+        private double area;
+        private double side;
+
+        public Square(double side) {
+            this.side = side;
+        }
+
+        @Override
+        public String toString() {
+            return "Square{" +
+                    "perimeter=" + perimeter +
+                    ", area=" + area +
+                    '}';
+        }
+
+        @Override
+        public double area() {
+            area = side * side;
+            return area;
+        }
+
+        @Override
+        public double perimeter() {
+            return perimeter = side + side + side + side;
+        }
+
+        @Override
+        public int compare(Square o1, Square o2) {
+            return (int) (o1.area()-o2.area());
+
+        }
+    }
