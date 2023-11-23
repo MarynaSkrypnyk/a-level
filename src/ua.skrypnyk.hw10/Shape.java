fac68636696e5b561c71d227a1b7bf120d6b1355
MarynaSkrypnyk <Marikaya.1997@gmail.com>
2023-11-23 23:07:33 +0200

    HW13 Comparator

---
+++ src/ua.skrypnyk.hw10/Shape.java
@@ -0,0 +1,8 @@
+package src.ua.skrypnyk.hw10;
+
+public abstract class Shape {
+
+    public abstract void perimeter ();
+
+    public abstract double area ();
+}
---
+++ src/ua.skrypnyk.hw10/Triangle.java
@@ -0,0 +1,57 @@
+package src.ua.skrypnyk.hw10;
+
+public class Triangle extends Shape {
+    private double perimeter;
+    private double area;
+    int sideA;
+    int sideB;
+    int sideC;
+    double height;
+
+    public Triangle(int sideA, int sideB, int sideC,double height) {
+        this.sideA = sideA;
+        this.sideB = sideB;
+        this.sideC = sideC;
+        this.height = height;
+    }
+
+    @Override
+    public void perimeter() {
+        double perimeter = sideA + sideB + sideC;
+        System.out.println("Triangle perimeter = "+ perimeter);
+    }
+
+    @Override
+    public double area() {
+        System.out.println("areaTriangleForHeronFormula = "+ area(16, 17, 19));
+        System.out.println("areaTriangleForTwoSidesPerSineAngle = " + area(16,17));
+        System.out.println("areaTriangleForBaseHeight = " + area(16, 17.2));
+        return area;
+    }
+
+    public Triangle(double area) {
+        this.area = area;
+    }
+
+    public double area (int sideA, int sideB, int sideC) {
+        double perimeter = (sideA + sideB + sideC) / (double) 2;
+        return area  = Math.sqrt(perimeter * ((perimeter - sideA) * (perimeter - sideB) * (perimeter - sideC)));
+    }
+
+    public double area (int sideA, int sideB) {
+        return area = Math.sqrt(1/(double)2) * (sideA * sideB * Math.sin(45));
+    }
+
+    public double area (int sideA, double height){
+        return area = 1/(double)2 * (sideA * height);
+
+   }
+}
+
+
+
+
+
+
+
+
