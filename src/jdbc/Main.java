306a256a275bea743a826410eb160ba6fe1127de
MarynaSkrypnyk <Marikaya.1997@gmail.com>
2023-12-24 11:41:09 +0200

    sql-skrypnyk- work on mistake

--- src/jdbc/Main.java
+++ src/jdbc/Main.java
@@ -1,32 +1,43 @@
 package src.jdbc;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+
 public class Main {
-    public static void main(String[] args) {
-        try (Connection connection = ConnectionManager.getConnection()){
+    public static void main(String[] args) throws SQLException {
+        Connection connection = ConnectionManager.getConnection();
+        try {
             Statement statement = connection.createStatement();
-
-            String sqlQuery = """
-                     insert into contact
-                     (contact, contact_type)
-                     VALUES ('Andy', 'Andy')
-                     """;
-            int i = statement.executeUpdate(sqlQuery);
-            System.out.println(i);
-
-            String allContact = """
-                     select * from contact;
-                     """;
-            ResultSet resultList = statement.executeQuery(allContact);
-            while (resultList.next()) {
-                System.out.println(resultList.getString("contact"));
-            }
-        }catch (SQLException e){
+        } catch (SQLException e) {
             throw new RuntimeException(e);
+        }
+        String query = """
+                insert into contact
+                (contact, contact_type)
+                VALUES (?, ?)
+                """;
 
+        String allContact = """
+               select * from contact;
+               """;
+        addNewContact(connection, query,"Hide","Hide");
+        addNewContact(connection, query,"Java","Java");
+        selectAllContact(connection, allContact);
+    }
+
+    private static void addNewContact(Connection connection, String query, String contact, String contact_type) throws SQLException {
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setString(1, contact);
+        preparedStatement.setString(2, contact_type);
+        preparedStatement.executeUpdate();
+    }
+
+    private static void selectAllContact(Connection connection, String allContact) throws SQLException {
+        PreparedStatement preparedStatement = connection.prepareStatement(allContact);
+        ResultSet resultList = preparedStatement.executeQuery();
+        while (resultList.next()) {
+            System.out.println(resultList.getString("contact"));
+            System.out.println(resultList.getString("contact_type"));
+            System.out.println("--------");
         }
     }
 }
